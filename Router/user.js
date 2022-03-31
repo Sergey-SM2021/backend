7b451784b9b68fb2f5bb00708fbb8ffed937d1b1
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-03-31 16:49:44 +0300

    made find freelancer in users table

--- Router/user.js
+++ Router/user.js
@@ -1,6 +1,21 @@
 import express from "express"
-import logic from "../Controllers/client.js"
+import { pool } from '../db.js'
 
 const server = express.Router()
+
+server.get('', async (req, res) => {
+    try {
+        const { mail, password } = req.query
+        const USER = await (await pool.query(`SELECT type, id FROM USERS where mail = '${mail}' and password = '${password}'`)).rows[0]
+        switch (USER.type) {
+            case "freelancers":
+                res.redirect(`/freelancer/${USER.id}`)
+        }
+        res.send(USER)
+    } catch (error) {
+        console.log(error)
+    }
+
+})
 
 export default server
