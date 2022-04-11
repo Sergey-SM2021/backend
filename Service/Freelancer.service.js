aec40b7fe0240e8ab9b5c54354567a86061615f2
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-04-11 15:19:47 +0300

    put request to work history

--- Service/Freelancer.service.js
+++ Service/Freelancer.service.js
@@ -31,10 +31,13 @@ class Freelancer {
 
     updateFreelancer = async (id, freelancer) => {
         try {
-            console.log(`------------------------------------------- ${freelancer}`)
-            freelancer.stack.forEach(async skill => {
+            await freelancer.stack.forEach(async skill => {
                 await pool.query(`delete from stack where freelancer = ${id}`)
                 await pool.query(`insert into stack (name,freelancer) values('${skill.name}',${id}) `)
+            })
+            await freelancer.workhistory.forEach(async work => {
+                await pool.query(`delete from workhistory where freelancer = ${id}`)
+                await pool.query(`insert into workhistory (name,price,freelancer) values('${work.name}',${work.price},${id}) `)
             })
             const updatedFreelancer = await pool.query(`update freelancers set name = '${freelancer.name}',
             lastname = '${freelancer.lastname}',
@@ -42,7 +45,7 @@ class Freelancer {
             expiriens = '${freelancer.expiriens}', price = '${freelancer.price}', paymentmethod = '${freelancer.paymentmethod}'
             where id = ${id}`)
         } catch (error) {
-            throw error
+            console.log(error)
         }
     }
 }
